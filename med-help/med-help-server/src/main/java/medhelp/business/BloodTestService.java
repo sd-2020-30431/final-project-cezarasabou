fe50c5afe984fa7eb1c50fa4ea089bd8e46d1e78
cezarasabou <s_cezara@yahoo.com>
2020-05-27 01:33:49 +0300

    created controllers

---
+++ med-help/med-help-server/src/main/java/medhelp/business/BloodTestService.java
@@ -0,0 +1,9 @@
+package medhelp.business;
+
+import org.springframework.stereotype.Service;
+
+import javax.websocket.server.ServerEndpoint;
+
+@Service
+public class BloodTestService {
+}
---
+++ med-help/med-help-server/src/main/java/medhelp/business/ComorbiditiesService.java
@@ -0,0 +1,7 @@
+package medhelp.business;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ComorbiditiesService {
+}
