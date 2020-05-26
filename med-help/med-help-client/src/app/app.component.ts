70d881bb990621cde432f4da34fb3ade02515c4c
cezarasabou <s_cezara@yahoo.com>
2020-05-26 21:24:38 +0300

    commit

---
+++ med-help/med-help-client/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {UserService} from "./services/user.service";
+import {User} from "./user";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'med-help-client';
+  constructor(private userService: UserService) { }
+
+  users: User[];
+  ngOnInit(): void {
+    this.userService.getUsersList().subscribe(users => {
+      this.users = users;
+    });
+  }
+}
---
+++ med-help/med-help-server/src/test/java/medhelp/MedHelpApplicationTests.java
@@ -0,0 +1,13 @@
+package medhelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
