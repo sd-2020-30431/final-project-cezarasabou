1de152d9e184376bf7f10124a896eb02aa464365
cezarasabou <s_cezara@yahoo.com>
2020-05-27 01:14:29 +0300

    created repos for all data

--- med-help/med-help-server/src/main/java/medhelp/model/Symptom.java
+++ med-help/med-help-server/src/main/java/medhelp/model/Symptom.java
@@ -1,18 +1,37 @@
 package medhelp.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Symptom {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long symptom_id;
+
+    @Column(name = "symptom_name", nullable = false)
     private int symptomName;
+
+    public Symptom() {
+
+    }
+
+    public long getSymptom_id() {
+        return symptom_id;
+    }
+
+    public void setSymptom_id(long symptom_id) {
+        this.symptom_id = symptom_id;
+    }
 
     public int getSymptomName() {
         return symptomName;
     }
 
     public void setSymptomName(int symptomName) {
+
         this.symptomName = symptomName;
     }
 
-    public Symptom(int symptomName) {
-        this.symptomName = symptomName;
-    }
+
 }
---
+++ med-help/med-help-server/src/main/java/medhelp/repository/XRayRepository.java
@@ -0,0 +1,9 @@
+package medhelp.repository;
+
+import medhelp.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface XRayRepository extends JpaRepository<User, Long> {
+}
